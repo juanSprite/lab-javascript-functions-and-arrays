61661a8ae19a006451783381720bb2afd18a1ada
Juan Enterria <ianiusovi@gmail.com>
2023-10-10 16:09:18 +0200

    final

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -15,18 +15,17 @@ const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard',
 function findLongestWord(array) {
   if (array = []){
     return null
-  }
-  if (array.length === 1) {
+  }else if (array.length === 1) {
     return array[0]
-  }
+  } else {
+  let longestWord = ""
   for(i=0; i < array.length-1; i++ ){
-    if(array.length === 1){
-      return array[i]
-    }
+    if(array[i].length < array[i+1].length)
+    longestWord = array[i+1]
   }
-
+  return longestWord
+ }
 }
-
 
 
 // Iteration #3: Calculate the sum
